a4a5de722da98259830b5d91aebc3a1bacd32bf4
Fred Weigel <fred_weigel@hotmail.com>
2021-07-18 14:59:34 -0400

    Use stdint.h per EtchedPixels

--- types.h
+++ types.h
@@ -11,23 +11,25 @@
 
 /* Types for Hi-Tech C, z80, 8 bit
  */
-typedef unsigned char  uint8;
-typedef unsigned int   uint16;
-typedef unsigned long  uint32;
-typedef signed char    int8;
-typedef int            int16;
-typedef long           int32;
+#define uint8  unsigned char
+#define uint16 unsigned int
+#define uint32 unsigned long
+#define int8   signed char
+#define int16  int
+#define int32  long
 
 #else
 
 /* Types for GCC/TCC, Linux, 64 bit
  */
-typedef unsigned char  uint8;
-typedef unsigned short uint16;
-typedef unsigned int   uint32;
-typedef signed char    int8;
-typedef short          int16;
-typedef int            int32;
+#include <stdint.h>
+
+#define uint8  uint8_t
+#define uint16 uint16_t
+#define uint32 uint32_t
+#define int8   int8_t
+#define int16  int16_t
+#define int32  int32_t
 
 #endif
 
